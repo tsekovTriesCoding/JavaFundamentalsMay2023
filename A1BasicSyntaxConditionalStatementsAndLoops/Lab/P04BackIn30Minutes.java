3de8301336f7baf55db92de1353f9f72fe8eae50
figtereiscoding <quererqabv@gmail.com>
2023-06-27 16:34:21 +0300

    Create P04BackIn30Minutes.java

---
+++ A1BasicSyntaxConditionalStatementsAndLoops/Lab/P04BackIn30Minutes.java
@@ -0,0 +1,24 @@
+package A1BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class P04BackIn30Minutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int hourIn = Integer.parseInt(scanner.nextLine());
+        int minutesIn = Integer.parseInt(scanner.nextLine());
+
+        int totalMinutes = minutesIn + hourIn * 60 + 30;
+        int hour = totalMinutes / 60;
+        int minutes = totalMinutes % 60;
+
+        if (hour > 23) {
+            hour = 0;
+        }
+
+        String output = String.format("%d:%02d", hour, minutes);
+
+        System.out.println(output);
+    }
+}
