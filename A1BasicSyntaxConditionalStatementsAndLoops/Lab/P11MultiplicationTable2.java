0340421580e4361bcf01811b63614bcb9c8aff06
figtereiscoding <quererqabv@gmail.com>
2023-07-01 22:00:45 +0300

    Create P11MultiplicationTable2.java

---
+++ A1BasicSyntaxConditionalStatementsAndLoops/Lab/P11MultiplicationTable2.java
@@ -0,0 +1,26 @@
+package A1BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class P11MultiplicationTable2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int multiplier = Integer.parseInt(scanner.nextLine());
+
+        if (multiplier > 10) {
+            System.out.printf("%d X %d = %d", n, multiplier, n * multiplier);
+        }
+
+        while (multiplier <= 10) {
+            int result = 0;
+            result = n * multiplier;
+
+            System.out.printf("%d X %d = %d%n", n, multiplier, result);
+
+            multiplier++;
+        }
+
+    }
+}
