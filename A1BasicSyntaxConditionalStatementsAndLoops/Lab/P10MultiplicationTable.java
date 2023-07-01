caa0664140dfa18f57d02e9b89fa013af5a97815
figtereiscoding <quererqabv@gmail.com>
2023-07-01 22:00:31 +0300

    Create P10MultiplicationTable.java

---
+++ A1BasicSyntaxConditionalStatementsAndLoops/Lab/P10MultiplicationTable.java
@@ -0,0 +1,23 @@
+package A1BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class P10MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int multiplier = 1;
+
+
+        while (multiplier <= 10) {
+            int result = 0;
+            result = n * multiplier;
+
+            System.out.printf("%d X %d = %d%n", n, multiplier, result);
+
+            multiplier++;
+        }
+    }
+}
