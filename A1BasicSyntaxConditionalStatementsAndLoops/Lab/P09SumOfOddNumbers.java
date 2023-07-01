77f9c7d5d975a8beabca0bf39a89c2aa5077290f
figtereiscoding <quererqabv@gmail.com>
2023-07-01 22:00:19 +0300

    Create P09SumOfOddNumbers.java

---
+++ A1BasicSyntaxConditionalStatementsAndLoops/Lab/P09SumOfOddNumbers.java
@@ -0,0 +1,21 @@
+package A1BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class P09SumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+
+        for (int i = 1; i <= 2 * n; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+                sum = sum + i;
+            }
+        }
+        System.out.printf("Sum: %d", sum);
+    }
+}
