5c609781e892110d0a391be6d66f8c758f35e740
figtereiscoding <quererqabv@gmail.com>
2023-07-04 22:11:13 +0300

    Create P01Ages.java

---
+++ A1BasicSyntaxConditionalStatementsAndLoops/Exercise/P01Ages.java
@@ -0,0 +1,26 @@
+package A2ExerciseBasicSyntaxConditionalStatementsAndLoops.Exercises;
+
+import java.util.Scanner;
+
+public class P01Ages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int age = Integer.parseInt(scanner.nextLine());
+        String personType = "";
+
+        if (age >= 0 && age <= 2) {
+            personType = "baby";
+        } else if (age > 2 && age <= 13) {
+            personType = "child";
+        } else if (age > 13 && age <= 19) {
+            personType = "teenager";
+        } else if (age > 19 && age <= 65) {
+            personType = "adult";
+        } else if (age > 65) {
+            personType = "elder";
+        }
+
+        System.out.println(personType);
+    }
+}
