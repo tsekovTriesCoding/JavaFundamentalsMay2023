94f5248a1e3c9983f034119460e4a3f7e343ceb6
figtereiscoding <quererqabv@gmail.com>
2023-07-04 22:12:19 +0300

    Create P08TriangleOfNumbers.java

---
+++ A1BasicSyntaxConditionalStatementsAndLoops/Exercise/P08TriangleOfNumbers.java
@@ -0,0 +1,22 @@
+package A2ExerciseBasicSyntaxConditionalStatementsAndLoops.Exercises;
+
+import java.util.Scanner;
+
+public class P08TriangleOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        for (int rows = 1; rows <= num ; rows++) {
+            for (int cols = 1; cols <= rows ; cols++) {
+
+                System.out.print(rows);
+                if (cols < rows) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
