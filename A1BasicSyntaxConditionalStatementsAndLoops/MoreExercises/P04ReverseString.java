01c8c0cc6317bde2271920364aeb9cf17767e4cf
figtereiscoding <quererqabv@gmail.com>
2023-07-05 16:27:53 +0300

    Create P04ReverseString.java

---
+++ A1BasicSyntaxConditionalStatementsAndLoops/MoreExercises/P04ReverseString.java
@@ -0,0 +1,17 @@
+package A2ExerciseBasicSyntaxConditionalStatementsAndLoops.MoreExercises;
+
+import java.util.Scanner;
+
+public class P04ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String reverseInput = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reverseInput += input.charAt(i);
+        }
+        System.out.println(reverseInput);
+    }
+}
