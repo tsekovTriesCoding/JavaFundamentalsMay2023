8689ed7c21d7b1fa6496259792f917799c199e16
figtereiscoding <quererqabv@gmail.com>
2023-07-11 17:51:06 +0300

    Create P08LowerOrUpper.java

---
+++ A2DataTypesAndVariables/Lab/P08LowerOrUpper.java
@@ -0,0 +1,17 @@
+package A3DataTypeAndVariables;
+
+import java.util.Scanner;
+
+public class P08LowerOrUpper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char symbol = scanner.nextLine().charAt(0);
+
+        if (Character.isUpperCase(symbol)) {
+            System.out.println("upper-case");
+        } else if (Character.isLowerCase(symbol)) {
+            System.out.println("lower-case");
+        }
+    }
+}
