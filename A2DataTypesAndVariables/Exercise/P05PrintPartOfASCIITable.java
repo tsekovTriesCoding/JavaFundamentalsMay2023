1c466c1b7df799598b1bb8f495ef27f49fb7122d
figtereiscoding <quererqabv@gmail.com>
2023-07-25 18:20:26 +0300

    Create P05PrintPartOfASCIITable.java

---
+++ A2DataTypesAndVariables/Exercise/P05PrintPartOfASCIITable.java
@@ -0,0 +1,16 @@
+package A4ExerciseDataTypesAndVariables.Exercise;
+
+import java.util.Scanner;
+
+public class P05PrintPartOfASCIITable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int indexStart = Integer.parseInt(scanner.nextLine());
+        int indexEnd = Integer.parseInt(scanner.nextLine());
+
+        for (int i = indexStart; i <= indexEnd; i++) {
+            System.out.print((char) i + " ");
+        }
+    }
+}
