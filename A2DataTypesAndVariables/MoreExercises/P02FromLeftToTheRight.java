72e67731a72eeaadb56345176b34b9f6547d1e72
figtereiscoding <quererqabv@gmail.com>
2023-07-25 18:22:16 +0300

    Create P02FromLeftToTheRight.java

---
+++ A2DataTypesAndVariables/MoreExercises/P02FromLeftToTheRight.java
@@ -0,0 +1,33 @@
+package A4ExerciseDataTypesAndVariables.MoreExercises;
+
+import java.util.Scanner;
+
+public class P02FromLeftToTheRight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            String numbers = scanner.nextLine();
+
+            String[] nums = numbers.split(" ");
+            String num1 = nums[0];
+            String num2 = nums[1];
+
+            long num1Parsed = Long.parseLong(num1);
+            long num2Parsed = Long.parseLong(num2);
+
+            long biggestNum = Math.max(num1Parsed, num2Parsed);
+
+            int sumOfDigits = 0;
+            while (biggestNum != 0) {
+                sumOfDigits += biggestNum % 10;
+                biggestNum /= 10;
+            }
+
+            System.out.println(Math.abs(sumOfDigits));
+        }
+    }
+}
+
