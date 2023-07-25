971e11fb3ef5307d562730e1d1c114db59d1bd13
figtereiscoding <quererqabv@gmail.com>
2023-07-25 18:21:20 +0300

    Create P10PokeMon.java

---
+++ A2DataTypesAndVariables/Exercise/P10PokeMon.java
@@ -0,0 +1,27 @@
+package A4ExerciseDataTypesAndVariables.Exercise;
+
+import java.util.Scanner;
+
+public class P10PokeMon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int pokePower = Integer.parseInt(scanner.nextLine());
+        int distance = Integer.parseInt(scanner.nextLine());
+        int exhaustionFactor = Integer.parseInt(scanner.nextLine());
+
+        int targetsHit = 0;
+        int startPokePower = pokePower;
+
+        while (pokePower >= distance) {
+            pokePower -= distance;
+            targetsHit++;
+
+            if (pokePower == startPokePower / 2 && exhaustionFactor != 0) {
+                pokePower /= exhaustionFactor;
+            }
+        }
+        System.out.println(pokePower);
+        System.out.println(targetsHit);
+    }
+}
