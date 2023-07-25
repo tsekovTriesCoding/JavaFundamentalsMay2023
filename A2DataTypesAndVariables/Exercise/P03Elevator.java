4c780eec1d2ecc3c1363d01f63112a016e3b4dee
figtereiscoding <quererqabv@gmail.com>
2023-07-25 18:20:06 +0300

    Create P03Elevator.java

---
+++ A2DataTypesAndVariables/Exercise/P03Elevator.java
@@ -0,0 +1,20 @@
+package A4ExerciseDataTypesAndVariables.Exercise;
+
+import java.util.Scanner;
+
+public class P03Elevator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfPeople = Integer.parseInt(scanner.nextLine());
+        int elevatorCapacity = Integer.parseInt(scanner.nextLine());
+
+        //int fullCourses = numberOfPeople / elevatorCapacity;
+        //if (numberOfPeople % elevatorCapacity != 0) {
+        //   fullCourses++;
+        //   }
+
+        int courses = (int) Math.ceil((double) numberOfPeople / elevatorCapacity);
+        System.out.println(courses);
+    }
+}
