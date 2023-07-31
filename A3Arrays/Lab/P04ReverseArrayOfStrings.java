67f333edb38050a092fa450e35b345cf525e64eb
figtereiscoding <quererqabv@gmail.com>
2023-07-31 16:38:54 +0300

    Create P04ReverseArrayOfStrings.java

---
+++ A3Arrays/Lab/P04ReverseArrayOfStrings.java
@@ -0,0 +1,21 @@
+package A5Arrays;
+
+import java.util.Scanner;
+
+public class P04ReverseArrayOfStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] stringArray = scanner.nextLine().split(" ");
+
+        for (int i = 0; i < stringArray.length / 2; i++) {
+            String replacedElement = stringArray[i];
+            stringArray[i] = stringArray[stringArray.length - 1 - i];
+
+            stringArray[stringArray.length - 1 - i] = replacedElement;
+
+        }
+        System.out.println(String.join(" ", stringArray));
+
+    }
+}
