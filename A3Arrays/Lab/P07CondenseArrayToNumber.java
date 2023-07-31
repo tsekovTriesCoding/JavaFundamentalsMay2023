f0bb3580de6c790fbf27f5d56340f49c55ce3da7
figtereiscoding <quererqabv@gmail.com>
2023-07-31 16:39:21 +0300

    Create P07CondenseArrayToNumber.java

---
+++ A3Arrays/Lab/P07CondenseArrayToNumber.java
@@ -0,0 +1,29 @@
+package A5Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P07CondenseArrayToNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = 0;
+        while (numbers.length > 1) {
+            int[] condensedNums = new int[numbers.length - 1];
+            for (int i = 0; i < numbers.length - 1; i++) {
+
+                condensedNums[i] = numbers[i] + numbers[i + 1];
+
+
+            }
+            numbers = condensedNums;
+        }
+        sum = Arrays.stream(numbers).sum();
+        System.out.println(sum);
+    }
+}
