18e0e31ba215d500261cd11a6bff4f23d66f3a34
figtereiscoding <quererqabv@gmail.com>
2023-07-31 16:38:25 +0300

    Create P01DayOfWeek.java

---
+++ A3Arrays/Lab/P01DayOfWeek.java
@@ -0,0 +1,20 @@
+package A5Arrays;
+
+import java.util.Scanner;
+
+public class P01DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] daysOFWeek = {"Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday", "Sunday"};
+
+        int day = Integer.parseInt(scanner.nextLine());
+
+        if (day >= 1 && day <= 7) {
+            System.out.println(daysOFWeek[day - 1]);
+        } else {
+            System.out.println("Invalid day!");
+        }
+    }
+}
