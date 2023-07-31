4dec9d641fa2ad8d05ba5af365a171c07b362b20
figtereiscoding <quererqabv@gmail.com>
2023-07-31 16:40:51 +0300

    Create P08MagicSum.java

---
+++ A3Arrays/Exercise/P08MagicSum.java
@@ -0,0 +1,26 @@
+package A6ExerciseArrays.Exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P08MagicSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int magicNum = Integer.parseInt(scanner.nextLine());
+
+        for (int index = 0; index <= numbers.length - 1; index++) {
+            for (int nextIndex = index + 1; nextIndex <= numbers.length - 1; nextIndex++) {
+                if (numbers[index] + numbers[nextIndex] == magicNum) {
+                    System.out.print(numbers[index] + " " + numbers[nextIndex]);
+                    System.out.println();
+                    break;
+                }
+            }
+        }
+    }
+}
