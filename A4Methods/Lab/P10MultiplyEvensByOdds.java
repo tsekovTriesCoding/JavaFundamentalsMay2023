b7fa29385acc068a0bd04ae997ce0eeac0e29bac
figtereiscoding <quererqabv@gmail.com>
2023-08-01 23:08:47 +0300

    Create P10MultiplyEvensByOdds.java

---
+++ A4Methods/Lab/P10MultiplyEvensByOdds.java
@@ -0,0 +1,34 @@
+package A8Methods;
+
+import java.util.Scanner;
+
+public class P10MultiplyEvensByOdds {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(multiplyEvensByOdds(number));
+    }
+
+    public static int multiplyEvensByOdds(int number) {
+
+        int oddDigitsSum = 0;
+        int evenDigitsSum = 0;
+        number = Math.abs(number);
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+
+            if (lastDigit % 2 == 0) {
+                evenDigitsSum += lastDigit;
+            } else {
+                oddDigitsSum += lastDigit;
+            }
+
+            number = number / 10;
+        }
+
+        return evenDigitsSum * oddDigitsSum;
+    }
+}
