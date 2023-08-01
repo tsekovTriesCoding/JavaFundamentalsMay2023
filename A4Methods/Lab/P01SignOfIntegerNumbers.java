0f85119d23dc6c3626262bee68f9df74de9ddeac
figtereiscoding <quererqabv@gmail.com>
2023-08-01 23:05:24 +0300

    Create P01SignOfIntegerNumbers.java

---
+++ A4Methods/Lab/P01SignOfIntegerNumbers.java
@@ -0,0 +1,25 @@
+package A8Methods;
+
+import java.util.Scanner;
+
+public class P01SignOfIntegerNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        printNumSign(number);
+    }
+
+    public static void printNumSign(int num) {
+
+        if (num > 0) {
+            System.out.printf("The number %d is positive.", num);
+        } else if (num < 0) {
+            System.out.printf("The number %d is negative.", num);
+        } else {
+            System.out.printf("The number %d is zero.", num);
+        }
+    }
+}
+
