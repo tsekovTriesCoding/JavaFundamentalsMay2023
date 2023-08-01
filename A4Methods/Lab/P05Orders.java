8175a7c976e635222fa8ef41dac75fda820a3840
figtereiscoding <quererqabv@gmail.com>
2023-08-01 23:05:55 +0300

    Create P05Orders.java

---
+++ A4Methods/Lab/P05Orders.java
@@ -0,0 +1,32 @@
+package A8Methods;
+
+import java.util.Scanner;
+
+public class P05Orders {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String product = scanner.nextLine();
+        int quantity = Integer.parseInt(scanner.nextLine());
+
+        printOrderPrice(product,quantity);
+
+    }
+
+    public static void printOrderPrice(String product, int quantity) {
+
+        double price = 0;
+        if (product.equals("coffee")) {
+            price = 1.50;
+        } else if (product.equals("water")) {
+            price = 1;
+        } else if (product.equals("coke")) {
+            price = 1.40;
+        } else if (product.equals("snacks")) {
+            price = 2;
+        }
+
+        price = price * quantity;
+        System.out.printf("%.2f", price);
+    }
+}
