f5966c35ae26a3f3ac9df2517bc1e3dc4faedc6b
figtereiscoding <quererqabv@gmail.com>
2023-08-01 23:03:10 +0300

    Create P03RecursiveFibonacci.java

---
+++ A3Arrays/MoreExercises/P03RecursiveFibonacci.java
@@ -0,0 +1,26 @@
+package A6ExerciseArrays.MoreExercises;
+
+import java.util.Scanner;
+
+public class P03RecursiveFibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int[] fibonacci = new int[n + 1];
+
+        fibonacci[0] = 1;
+        int f0 = 0;
+        int f1 = 1;
+
+        for (int i = 0; i < n; i++) {
+            int nF = f0 + f1;
+            f0 = f1;
+            f1 = nF;
+            fibonacci[i + 1] = f1;
+        }
+
+        System.out.print(fibonacci[fibonacci.length - 2]);
+    }
+}
