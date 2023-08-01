90789d58c2704a9cf9f2c41fb3527a75d7a20d6c
figtereiscoding <quererqabv@gmail.com>
2023-08-01 23:08:20 +0300

    Create P07RepeatString.java

---
+++ A4Methods/Lab/P07RepeatString.java
@@ -0,0 +1,23 @@
+package A8Methods;
+
+import java.util.Scanner;
+
+public class P07RepeatString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        int n = Integer.parseInt(scanner.nextLine());
+
+        String output = repeatString(input, n);
+        System.out.println(output);
+    }
+
+    public static String repeatString(String input, int n) {
+        String repeatedString = "";
+        for (int i = 1; i <= n; i++) {
+            repeatedString += input;
+        }
+        return repeatedString;
+    }
+}
