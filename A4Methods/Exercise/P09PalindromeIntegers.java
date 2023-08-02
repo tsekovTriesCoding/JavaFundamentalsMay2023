c570b41c46196cf7c6e558f439f96ab33264a58a
figtereiscoding <quererqabv@gmail.com>
2023-08-02 15:08:10 +0300

    Create P09PalindromeIntegers.java

---
+++ A4Methods/Exercise/P09PalindromeIntegers.java
@@ -0,0 +1,30 @@
+package A9ExerciseMethods.Exercises;
+
+import java.util.Scanner;
+
+public class P09PalindromeIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+
+        while (!command.equals("END")) {
+            if (isPalindrome(command)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+
+            command = scanner.nextLine();
+        }
+    }
+
+    public static boolean isPalindrome(String command) {
+        String reversedCommand = "";
+
+        for (int index = command.length() - 1; index >= 0; index--) {
+            reversedCommand += command.charAt(index);
+        }
+        return command.equals(reversedCommand);
+    }
+}
