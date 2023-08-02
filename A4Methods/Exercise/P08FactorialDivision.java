21d863ee946e72175f7da52f63d25cf116e2fd2c
figtereiscoding <quererqabv@gmail.com>
2023-08-02 15:08:01 +0300

    Create P08FactorialDivision.java

---
+++ A4Methods/Exercise/P08FactorialDivision.java
@@ -0,0 +1,26 @@
+package A9ExerciseMethods.Exercises;
+
+import java.util.Scanner;
+
+public class P08FactorialDivision {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+
+        double result = calculateFactorial(num1) * 1.0 / calculateFactorial(num2);
+
+        System.out.printf("%.2f", result);
+    }
+
+    public static long calculateFactorial(int num) {
+
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+}
