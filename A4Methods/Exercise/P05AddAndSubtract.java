850755be5bfdad74ce42f3470ab515d033ce4ec8
figtereiscoding <quererqabv@gmail.com>
2023-08-02 15:07:35 +0300

    Create P05AddAndSubtract.java

---
+++ A4Methods/Exercise/P05AddAndSubtract.java
@@ -0,0 +1,22 @@
+package A9ExerciseMethods.Exercises;
+
+import java.util.Scanner;
+
+public class P05AddAndSubtract {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+        int num3 = Integer.parseInt(scanner.nextLine());
+
+        int result = sumOfTwoIntegers(num1, num2) - num3;
+
+        System.out.println(result);
+    }
+
+    public static int sumOfTwoIntegers(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
