b2c699d681f2f1df5ae677eadf6c69fcf1e33d2f
figtereiscoding <quererqabv@gmail.com>
2023-08-02 15:08:21 +0300

    Create P10TopNumber.java

---
+++ A4Methods/Exercise/P10TopNumber.java
@@ -0,0 +1,42 @@
+package A9ExerciseMethods.Exercises;
+
+import java.util.Scanner;
+
+public class P10TopNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= number; i++) {
+            if (isSumOFDigitsDivisibleBy8(i) && isHoldingOneOddDigit(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isSumOFDigitsDivisibleBy8(int num) {
+
+        int sumOfDigits = 0;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            sumOfDigits += lastDigit;
+            num = num / 10;
+        }
+        return sumOfDigits % 8 == 0;
+    }
+
+    public static boolean isHoldingOneOddDigit(int num) {
+
+        while (num > 0) {
+            int lastDigit = num % 10;
+
+            if (lastDigit % 2 != 0) {
+                return true;
+            } else {
+                num = num / 10;
+            }
+        }
+        return false;
+    }
+}
