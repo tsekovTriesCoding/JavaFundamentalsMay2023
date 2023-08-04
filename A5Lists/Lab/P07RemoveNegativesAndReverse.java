d8ba15bdd06e926f973d97da86cfa8cd4c9f1ba2
figtereiscoding <quererqabv@gmail.com>
2023-08-04 17:29:07 +0300

    Create P07RemoveNegativesAndReverse.java

---
+++ A5Lists/Lab/P07RemoveNegativesAndReverse.java
@@ -0,0 +1,31 @@
+package A10List;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class P07RemoveNegativesAndReverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Integer> positiveNumbers = new ArrayList<>();
+
+        for (int number : numbers) {
+            if (number >= 0) {
+                positiveNumbers.add(number);
+            }
+        }
+
+        Collections.reverse(positiveNumbers);
+
+        if (positiveNumbers.size() > 0) {
+            System.out.println(positiveNumbers.toString().replaceAll("[\\[\\],]", ""));
+
+        } else {
+            System.out.println("empty");
+        }
+    }
+}
