c41b46ebfdca9fc3b8d9b5f6a56ec029be3e6645
figtereiscoding <quererqabv@gmail.com>
2023-08-04 17:28:30 +0300

    Create P02GaussTrick.java

---
+++ A5Lists/Lab/P02GaussTrick.java
@@ -0,0 +1,27 @@
+package A10List;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P02GaussTrick {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            int firstNum = numbers.get(i);
+            int secondNum = numbers.get(numbers.size() - 1);
+
+            numbers.set(i, firstNum + secondNum);
+
+            numbers.remove(numbers.size() - 1);
+        }
+
+        System.out.println(numbers.toString().replaceAll("[\\[\\],]", ""));
+    }
+}
