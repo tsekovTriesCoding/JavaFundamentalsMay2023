d78b394ef648b36fa57e673dfea7cacaef4852e2
figtereiscoding <quererqabv@gmail.com>
2023-08-04 17:28:22 +0300

    Create P01SumAdjacentEqualNumbers.java

---
+++ A5Lists/Lab/P01SumAdjacentEqualNumbers.java
@@ -0,0 +1,38 @@
+package A10List;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P01SumAdjacentEqualNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Double> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < numbers.size() - 1; i++) {
+
+            if (numbers.get(i).equals(numbers.get(i + 1))) {
+                numbers.set(i, (numbers.get(i) + numbers.get(i + 1)));
+                numbers.remove(i + 1);
+                i = -1;
+            }
+        }
+
+        String output = joinElementsByDelimiter(numbers, " ");
+        System.out.println(output);
+    }
+
+    private static String joinElementsByDelimiter(List<Double> numbers, String delimiter) {
+        String output = "";
+        for (Double element : numbers) {
+            output += (new DecimalFormat("0.#").format(element) + delimiter);
+        }
+
+        return output;
+    }
+}
