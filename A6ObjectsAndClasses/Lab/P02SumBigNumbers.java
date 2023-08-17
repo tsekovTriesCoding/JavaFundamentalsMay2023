4bccc6a40ee31fbd29de8a4d57d4163bf055e341
figtereiscoding <quererqabv@gmail.com>
2023-08-17 23:53:58 +0300

    Create P02SumBigNumbers.java

---
+++ A6ObjectsAndClasses/Lab/P02SumBigNumbers.java
@@ -0,0 +1,17 @@
+package A13ObjectsAndClasses;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P02SumBigNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger num1 = new BigInteger(scanner.nextLine());
+        BigInteger num2 = new BigInteger(scanner.nextLine());
+
+        BigInteger sum = num1.add(num2);
+
+        System.out.println(sum);
+    }
+}
