23c0123c4eeab0216ba8a87a73fa4ce595f7e38b
figtereiscoding <quererqabv@gmail.com>
2023-08-18 17:08:31 +0300

    Create P04WordFilter.java

---
+++ A7AssociativeArrays/Lab/P04WordFilter.java
@@ -0,0 +1,17 @@
+package A15AssociativeArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P04WordFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] wordsArr = Arrays.stream(scanner.nextLine()
+                        .split("\\s+"))
+                .filter(w -> w.length() % 2 == 0)
+                .toArray(String[]::new);
+
+        System.out.println(String.join(System.lineSeparator(), wordsArr));
+    }
+}
