126629a05f7f6c5cd3c04feabb4362cf003cfb43
figtereiscoding <quererqabv@gmail.com>
2023-08-18 17:08:15 +0300

    Create P02WordSynonyms.java

---
+++ A7AssociativeArrays/Lab/P02WordSynonyms.java
@@ -0,0 +1,29 @@
+package A15AssociativeArrays;
+
+import java.util.*;
+
+public class P02WordSynonyms {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        LinkedHashMap<String, List<String>> synonymsMap = new LinkedHashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+
+            if (!synonymsMap.containsKey(word)) {
+                synonymsMap.put(word, new ArrayList<>());
+            }
+
+            synonymsMap.get(word).add(synonym);
+        }
+
+        for (Map.Entry<String, List<String>> entry : synonymsMap.entrySet()) {
+            System.out.printf("%s - %s%n", entry.getKey(), String.join(", ", entry.getValue()));
+        }
+
+    }
+}
