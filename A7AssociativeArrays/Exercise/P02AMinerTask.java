5ff667c6d48cfafd8bef28321af8292642468b5c
figtereiscoding <quererqabv@gmail.com>
2023-08-21 17:57:21 +0300

    Create P02AMinerTask.java

---
+++ A7AssociativeArrays/Exercise/P02AMinerTask.java
@@ -0,0 +1,30 @@
+package A16AssociativeArraysAndLambdaAndStreamAPI.Exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class P02AMinerTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+
+        LinkedHashMap<String, Integer> resourcesMap = new LinkedHashMap<>();
+
+        while (!command.equals("stop")) {
+            String resource = command;
+            int quantity = Integer.parseInt(scanner.nextLine());
+
+            if (!resourcesMap.containsKey(resource)) {
+                resourcesMap.put(resource, quantity);
+            } else {
+                int currentQuantity = resourcesMap.get(resource);
+                resourcesMap.put(resource, currentQuantity + quantity);
+            }
+
+            command = scanner.nextLine();
+        }
+
+        resourcesMap.forEach((key, value) -> System.out.println(key + " -> " + value));
+    }
+}
