9f5f9585507e7dfe0d625c66e5f60811d7b5a179
figtereiscoding <quererqabv@gmail.com>
2023-08-25 23:34:00 +0300

    Create P04TextFilter.java

---
+++ A8TextProcessing/Lab/P04TextFilter.java
@@ -0,0 +1,30 @@
+package A17TextProcessing;
+
+import java.util.Scanner;
+
+public class P04TextFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] banWordsArr = scanner.nextLine().split(", ");
+        String text = scanner.nextLine();
+
+        for (String banWord : banWordsArr) {
+            String asterisk = repeatAsterisk(banWord.length());
+
+            text = text.replace(banWord, asterisk);
+        }
+
+        System.out.println(text);
+    }
+
+    public static String repeatAsterisk(int n) {
+        String result = "";
+
+        for (int i = 1; i <= n; i++) {
+            result += "*";
+        }
+
+        return result;
+    }
+}
