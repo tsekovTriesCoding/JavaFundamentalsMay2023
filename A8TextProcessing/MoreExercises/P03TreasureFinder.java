f03fc8270c08d1388a57f68e15b6ecb30cc2b94c
figtereiscoding <quererqabv@gmail.com>
2023-08-25 23:36:03 +0300

    added 2 exercises more

---
+++ A8TextProcessing/MoreExercises/P03TreasureFinder.java
@@ -0,0 +1,58 @@
+package A18ExerciseTextProcessing.MoreExercise;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P03TreasureFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] keyArr = scanner.nextLine().split("\\s+");
+        String input = scanner.nextLine();
+
+        while (!input.equals("find")) {
+
+            StringBuilder sb = new StringBuilder();
+            boolean over = false;
+            for (int indexOfKey = 0; indexOfKey <= keyArr.length - 1; indexOfKey++) {
+                for (int indexOfString = 0; indexOfString <= input.length() - 1; indexOfString++) {
+                    int keyNum = Integer.parseInt(keyArr[indexOfKey]);
+                    char newSymbol = (char) (input.charAt(indexOfString) - keyNum);
+                    sb.append(newSymbol);
+                    indexOfKey++;
+                    if (indexOfKey == keyArr.length) {
+                        indexOfKey = 0;
+                    }
+                    if (indexOfString == input.length() - 1) {
+                        over = true;
+                    }
+                }
+                if (over) {
+                    break;
+                }
+            }
+            String treasureType = "";
+            String treasureTypeRegex = "&(?<type>[A-Za-z]+)&";
+            Pattern typePattern = Pattern.compile(treasureTypeRegex);
+            Matcher typeMatcher = typePattern.matcher(sb);
+
+            if (typeMatcher.find()) {
+                treasureType = typeMatcher.group("type");
+            }
+
+            String coordinates = "";
+            String coordinatesRegex = "<(?<coordinates>[A-Za-z\\d+]+)>";
+            Pattern coordinatesPattern = Pattern.compile(coordinatesRegex);
+            Matcher coordinatesMatcher = coordinatesPattern.matcher(sb);
+
+            if (coordinatesMatcher.find()) {
+                coordinates = coordinatesMatcher.group("coordinates");
+            }
+
+
+            System.out.printf("Found %s at %s%n", treasureType, coordinates);
+            input = scanner.nextLine();
+        }
+    }
+}
---
+++ A8TextProcessing/MoreExercises/P04MorseCodeTranslator.java
@@ -0,0 +1,37 @@
+package A18ExerciseTextProcessing.MoreExercise;
+
+import java.util.Scanner;
+
+public class P04MorseCodeTranslator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String message = scanner.nextLine();
+        String[] messageArr = message.split(" \\| " );
+
+        StringBuilder encryptedMessage = new StringBuilder();
+
+        String[] morseCodeArr = {".-", " -...", "-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.",
+                "---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        String[] englishLettersArr = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+                "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+        for (String morseCode : messageArr) {
+                String[] currentArr = morseCode.split("\\s+");
+
+                for (String currStr:currentArr) {
+                    for (int i = 0; i <morseCodeArr.length - 1 ; i++) {
+                        if (currStr.equals(morseCodeArr[i])) {
+                            encryptedMessage.append(englishLettersArr[i]);
+                            break;
+                        }
+
+                    }
+                }
+                encryptedMessage.append(" ");
+        }
+        System.out.println(encryptedMessage);
+
+    }
+}
