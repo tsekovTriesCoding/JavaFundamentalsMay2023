ec703d0d9d53186b3c9293e87ac061d02061066e
figtereiscoding <quererqabv@gmail.com>
2023-08-25 23:35:36 +0300

    Create P02AsciiSumator.java

---
+++ A8TextProcessing/MoreExercises/P02AsciiSumator.java
@@ -0,0 +1,24 @@
+package A18ExerciseTextProcessing.MoreExercise;
+
+import java.util.Scanner;
+
+public class P02AsciiSumator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char symbol1 = scanner.nextLine().charAt(0);
+        char symbol2 = scanner.nextLine().charAt(0);
+        String randomString = scanner.nextLine();
+
+        int asciiSum = 0;
+        for (int i = 0; i <= randomString.length() - 1; i++) {
+
+            if ((randomString.charAt(i) > symbol1 && randomString.charAt(i) < symbol2) ||
+                    (randomString.charAt(i) < symbol1 && randomString.charAt(i) > symbol2)) {
+                asciiSum += randomString.charAt(i);
+            }
+        }
+
+        System.out.println(asciiSum);
+    }
+}
