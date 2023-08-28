2c6f169ad9c2ee849dd6cc0ec2edebb8dab81c3d
figtereiscoding <quererqabv@gmail.com>
2023-08-28 17:39:02 +0300

    Create P01Furniture.java

---
+++ A9RegularExpressions/Exercise/P01Furniture.java
@@ -0,0 +1,40 @@
+package A20ExerciseRegularExpressions.Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P01Furniture {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String regex = ">>(?<furnitureName>[A-Za-z]+)<<(?<price>[0-9]+.?[0-9]+)!(?<quantity>[0-9]+)";
+        Pattern pattern = Pattern.compile(regex);
+        List<String> furnituresLIst = new ArrayList<>();
+        double totalCost = 0;
+
+        while (!input.equals("Purchase")) {
+            Matcher matcher = pattern.matcher(input);
+
+            if (matcher.find()) {
+                String furnitureName = matcher.group("furnitureName");
+                double price = Double.parseDouble(matcher.group("price"));
+                int quantity = Integer.parseInt(matcher.group("quantity"));
+
+                furnituresLIst.add(furnitureName);
+                totalCost += price * quantity;
+            }
+
+            input = scanner.nextLine();
+        }
+
+        System.out.println("Bought furniture:");
+        furnituresLIst.forEach(System.out::println);
+
+        System.out.printf("Total money spend: %.2f", totalCost);
+    }
+}
