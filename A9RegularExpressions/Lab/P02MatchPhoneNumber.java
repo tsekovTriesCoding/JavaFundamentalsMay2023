07c0ba4fe3edad7d3ca3688e7872629dd094c17c
figtereiscoding <quererqabv@gmail.com>
2023-08-28 17:38:03 +0300

    add 3 solutions for lab

---
+++ A9RegularExpressions/Lab/P02MatchPhoneNumber.java
@@ -0,0 +1,29 @@
+package A19RegularExpressions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class P02MatchPhoneNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String regex = "\\+359([ \\-])2\\1\\d{3}\\1\\d{4}\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> phonesList = new ArrayList<>();
+
+        while (matcher.find()) {
+            phonesList.add(matcher.group());
+        }
+
+        System.out.println(String.join(", ", phonesList));
+
+    }
+}
---
+++ A9RegularExpressions/Lab/P03MatchDates.java
@@ -0,0 +1,27 @@
+package A19RegularExpressions;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P03MatchDates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String regex = "\\b(?<day>\\d{2})([/.\\-])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher dates = pattern.matcher(input);
+
+        while (dates.find()) {
+            String day = dates.group("day");
+            String month = dates.group("month");
+            String year = dates.group("year");
+
+            System.out.printf("Day: %s, Month: %s, Year: %s%n", day, month, year);
+        }
+
+    }
+}
