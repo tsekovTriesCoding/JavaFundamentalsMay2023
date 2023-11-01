e5f2ac285ddbbfeb7dd021b42e284397d3a2144f
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-11-01 21:23:10 +0200

    added  exam preparation solutions

---
+++ A10ExamPreparation/A7MidExam/P01GuineaPig.java
@@ -0,0 +1,41 @@
+package A10ExamPreparation.A7MidExam;
+
+import java.util.Scanner;
+
+public class P01GuineaPig {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double foodQuantityInGrams = Double.parseDouble(scanner.nextLine()) * 1000;
+        double hayQuantityInGrams = Double.parseDouble(scanner.nextLine()) * 1000;
+        double quantityCoverInGrams = Double.parseDouble(scanner.nextLine()) * 1000;
+        double guineaPigWeightInGrams = Double.parseDouble(scanner.nextLine()) * 1000;
+
+        int day = 0;
+        while (foodQuantityInGrams > 0 && hayQuantityInGrams > 0 && quantityCoverInGrams > 0 && day < 30) {
+            day++;
+
+            foodQuantityInGrams -= 300;
+
+            if (day % 2 == 0) {
+                hayQuantityInGrams -= foodQuantityInGrams * 0.05;
+            }
+
+            if (day % 3 == 0) {
+                quantityCoverInGrams -= guineaPigWeightInGrams / 3;
+            }
+
+        }
+
+        boolean everythingIsEnough = foodQuantityInGrams > 0 && hayQuantityInGrams > 0 && quantityCoverInGrams > 0;
+
+        if (everythingIsEnough) {
+            System.out.printf("Everything is fine! Puppy is happy!" +
+                            " Food: %.2f, Hay: %.2f, Cover: %.2f.",
+                    foodQuantityInGrams / 1000, hayQuantityInGrams / 1000, quantityCoverInGrams / 1000);
+        } else {
+            System.out.println("Merry must go to the pet store!");
+        }
+
+    }
+}
---
+++ A10ExamPreparation/A7MidExam/P03HeartDelivery.java
@@ -0,0 +1,68 @@
+package A10ExamPreparation.A7MidExam;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P03HeartDelivery {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] neighborhood = Arrays
+                .stream(scanner.nextLine().split("@"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        String command = scanner.nextLine();
+        int lastPosition = 0;
+
+        while (!command.equals("Love!")) {
+            int jumpLength = Integer.parseInt(command.split(" ")[1]);
+
+            for (int i = lastPosition; i <= neighborhood.length - 1; i++) {
+                int indexToJumpTo = i + jumpLength;
+
+                if (indexToJumpTo > neighborhood.length - 1) {
+                    indexToJumpTo = 0;
+                }
+
+                if (neighborhood[indexToJumpTo] == 0) {
+                    System.out.printf("Place %d already had Valentine's day.%n", indexToJumpTo);
+                } else {
+                    neighborhood[indexToJumpTo] -= 2;
+
+                    if (neighborhood[indexToJumpTo] == 0) {
+                        System.out.printf("Place %d has Valentine's day.%n", indexToJumpTo);
+                    }
+                }
+
+                lastPosition = indexToJumpTo;
+                break;
+            }
+
+            command = scanner.nextLine();
+        }
+        System.out.printf("Cupid's last position was %d.%n", lastPosition);
+
+        boolean successful = true;
+        for (int element : neighborhood) {
+
+            if (element != 0) {
+                successful = false;
+                break;
+            }
+        }
+
+        if (successful) {
+            System.out.println("Mission was successful.");
+        } else {
+
+            int housesWithNoValentine = 0;
+            for (int element : neighborhood) {
+                if (element > 0) {
+                    housesWithNoValentine++;
+                }
+            }
+            System.out.printf("Cupid has failed %d places.", housesWithNoValentine);
+        }
+    }
+}
