57f37d4b2a411094818df7c7fcb733bdd8b39758
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-11-01 21:22:35 +0200

    added  exam preparation solutions

---
+++ A10ExamPreparation/A4MidExamRetake/P01ComputerStore.java
@@ -0,0 +1,44 @@
+package A10ExamPreparation.A4MidExamRetake;
+
+import java.util.Scanner;
+
+public class P01ComputerStore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+
+        double priceWithoutTaxes = 0;
+        double taxes = 0;
+        while (true) {
+            if (command.equals("regular") || command.equals("special")) {
+                break;
+            }
+            double partPrice = Double.parseDouble(command);
+
+            if (partPrice < 0) {
+                System.out.println("Invalid price!");
+            } else {
+                priceWithoutTaxes += partPrice;
+                taxes += partPrice * 0.2;
+            }
+            command = scanner.nextLine();
+        }
+
+        double totalPrice = priceWithoutTaxes + taxes;
+        if (command.equals("special")) {
+            totalPrice = totalPrice * 0.9;
+        }
+
+        if (totalPrice == 0) {
+            System.out.println("Invalid order!");
+        } else {
+            System.out.println("Congratulations you've just bought a new computer!");
+            System.out.printf("Price without taxes: %.2f$%n", priceWithoutTaxes);
+            System.out.printf("Taxes: %.2f$%n", taxes);
+            System.out.println("-----------");
+            System.out.printf("Total price: %.2f$", totalPrice);
+        }
+
+    }
+}
